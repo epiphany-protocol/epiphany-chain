31e47d10946cecfad8cecf36ce62cbceb1534b5c
nanfengpo <lsxxxxxx@126.com>
2022-06-16 15:16:43 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+git pull
 go build -o polygon-edge main.go
 mv polygon-edge /usr/local/bin
 scp polygon-edge root@8.219.62.228:/usr/local/bin
-scp polygon-edge root@8.219.129.249:/usr/local/bin
+scp polygon-edge root@8.219.120.249:/usr/local/bin
 scp polygon-edge root@8.219.143.221:/usr/local/bin
